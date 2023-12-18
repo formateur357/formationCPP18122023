de7b6c0d58bf91f21f8157075ef025bcb18fda2b
moguy93 <guymorgan@live.fr>
2023-12-18 09:53:11 +0100

    add exemple

---
+++ exemples/hello_world.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+// commentaire de ligne
+
+/*
+*** commentaire sur plusieurs lignes
+*/
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world !\n";
+
+    return 0;
+}
+
+// Afin de connaitre votre version de c++
+// int main()
+// {
+//     if (__cplusplus == 201703L)
+//         std::cout << "C++17\n";
+//     else if (__cplusplus == 201402L)
+//         std::cout << "C++14\n";
+//     else if (__cplusplus == 201103L)
+//         std::cout << "C++11\n";
+//     else if (__cplusplus == 199711L)
+//         std::cout << "C++98\n";
+//     else
+//         std::cout << "Unknown C++ version\n";
+//     return 0;
+// }
