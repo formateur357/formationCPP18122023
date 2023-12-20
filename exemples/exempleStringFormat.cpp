8d8fc822a9b4ad770b8cb5c1086df22692b3c26f
moguy93 <guymorgan@live.fr>
2023-12-20 09:07:31 +0100

    add exemple

---
+++ exemples/exempleStringFormat.cpp
@@ -0,0 +1,11 @@
+// c++20
+
+#include <format>
+#include <string>
+
+int main()
+{
+    int age = 30;
+    std::string formattedString = std::format("Age: {} ans", age);
+    // Utilisez formattedString
+}
